72384304ea86b8ed8683422a99d6da6c3355b4ac
wuzhicheng <godiszyh@outlook.com>
2018-03-31 15:19:23 +0800

    readme.md

--- README.md
+++ README.md
@@ -1,30 +1,139 @@
-# vue-elm
+# 前言
 
-> A Vue.js project
+一直想做个稍微复杂点的vue实战项目，逛论坛时看到有大牛做了个饿了么网站，就模仿了一个，所有的数据都是抓取他搭建的服务器上的，因为对webpack不太熟练，所以就用了vue-cli脚手架搭建项目。
 
-## Build Setup
+## 技术栈
 
-``` bash
-# install dependencies
+vue2 + vuex + vue-router + vue-resource + vue-cli + ES6/7 + sass + flex
+
+## 项目运行
+
+```
+git clone https://github.com/wapptaw/vue-elm.git
+
+cd vue-elm
+
 npm install
 
-# serve with hot reload at localhost:8080
 npm run dev
 
-# build for production with minification
-npm run build
-
-# build for production and view the bundle analyzer report
-npm run build --report
-
-# run unit tests
-npm run unit
-
-# run e2e tests
-npm run e2e
-
-# run all tests
-npm test
 ```
+## 功能
+* 定位功能
+* 选择城市
+* 搜多地址
+* 展示附近商家
+* 搜索店铺
+* 店铺分类展示
+* 店铺分类选择、排序
+* 店铺食品列表页
+* 店铺评价页
+* 商家详情页
+* 购物车功能
+* 登录、注册、改密码功能
+* 个人中心
+* 下单功能
+* 订单列表
+* 订单详情
+* 添加收货地址
+* 账户信息
 
-For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+# 总结
+
+1、不知什么原因，获取的图片验证码总是过期的，所以实际上是登陆不上的，不过不影响，页面照写。
+
+2、一些api失效了，所以有些地方不能使用，不过对于练手来说还是足够了。
+
+# 部分截图
+
+![1](https://github.com/wapptaw/vue-elm/blob/master/screen/20180331_144418.gif)
+![2](https://github.com/wapptaw/vue-elm/blob/master/screen/20180331_144621.gif)
+![3](https://github.com/wapptaw/vue-elm/blob/master/screen/20180331_145057.gif)
+![4](https://github.com/wapptaw/vue-elm/blob/master/screen/20180331_145222.gif)
+![5](https://github.com/wapptaw/vue-elm/blob/master/screen/20180331_145432.gif)
+![6](https://github.com/wapptaw/vue-elm/blob/master/screen/20180331_145658.gif)
+![7](https://github.com/wapptaw/vue-elm/blob/master/screen/20180331_145908.gif)
+![8](https://github.com/wapptaw/vue-elm/blob/master/screen/20180331_150428.gif)
+
+# 项目布局
+
+```
+.
+├── build                                               // webpack配置文件
+├── config                                              // webpack自行配置文件
+├── screen                                              // 部分截图
+├── src                                                 // 源码目录
+│   ├── components                                      // 组件
+│   │   └── common                                      // 公共组件
+│   │       ├── ballMove.vue                            // 购物移动球组件
+│   │       ├── BottomNav.vue                           // 底部导航组件
+│   │       ├── loading.vue                             // 等待动画组件
+│   │       ├── MatteOpacity.vue                        // 遮罩组件
+│   │       ├── PopUp.vue                               // 弹窗组件
+│   │       ├── promptBox.vue                           // 上拉下拉提示组件
+│   │       ├── scroll.vue                              // better-scroll组件
+│   │       ├── SearchBox.vue                           // 搜索窗组件
+│   │       ├── ShopList.vue                            // 商品列表组件
+│   │       └── TopBack.vue                             // 顶部标题栏组件
+│   ├── config                                          // 基本配置
+│   │   ├── fetch.js                                    // 数据获取函数
+│   │   ├── rem.js                                      // px和rem互转函数
+│   │   └── url.js                                      // url链接的开头
+│   ├── page
+│   │   ├── city
+│   │   │   ├── AddressSearch.vue                       // 地址搜索和选择
+│   │   │   └── CitySelect.vue                          // 城市搜索和选择
+│   │   ├── deliveryAddress
+│   │   │   ├── children
+│   │   │   │   ├── addressAdd.vue                      // 增加收获地址
+│   │   │   │   └── addressNearby.vue                   // 附近地址搜索
+│   │   │   └── deliveryAddress.vue                     // 收获地址列表
+│   │   ├── FoodPage
+│   │   │   ├── children
+│   │   │   │   ├── children
+│   │   │   │   │   └── shopSafe.vue                    // 店铺安全展示
+│   │   │   │   ├── evaluate.vue                        // 店铺评论
+│   │   │   │   ├── merchant.vue                        // 店铺详情
+│   │   │   │   └── order.vue                           // 店铺商品展示
+│   │   │   └── FoodPage.vue                            // 店铺页
+│   │   ├── forget
+│   │   │   └── resetPassword.vue                       // 重置密码
+│   │   ├── formPay
+│   │   │   ├── children
+│   │   │   │   ├── invoice.vue                         // 选择发票
+│   │   │   │   ├── payPage.vue                         // 付款页
+│   │   │   │   └── remark.vue                          // 预留信息
+│   │   │   └── OrderConfirm.vue                        // 订单确认页
+│   │   ├── Home
+│   │   │   ├── children
+│   │   │   │   ├── children
+│   │   │   │   │   ├── orderFormDetail.vue             // 订单详情
+│   │   │   │   │   └── userInfo.vue                    // 用户详情
+│   │   │   │   ├── OrderForm.vue                       // 订单列表
+│   │   │   │   ├── ShopSearch.vue                      // 店铺搜索
+│   │   │   │   ├── TakeOut.vue                         // 首页导航
+│   │   │   │   └── user.vue                            // 个人中心
+│   │   │   └── home.vue                                // home页
+│   │   ├── login
+│   │   │   └── login.vue                               // 登录/注册
+│   │   └── FoodCategory.vue                            // 店铺分类展示
+│   ├── plugin
+│   │   └── userInfo.js
+│   ├── router
+│   │   └── index.js                                    // 路由配置
+│   ├── service
+│   │   └── getData.js                                  // 数据获取
+│   ├── store                                           // vuex配置
+│   │   ├── actions.js
+│   │   ├── getters.js
+│   │   ├── index.js
+│   │   └── mutations.js
+│   ├── style
+│   │   ├── animation.scss                              // 全局动画样式
+│   │   └── init.scss                                   // 样式初始化
+│   ├── App.vue                                         // 页面入口
+│   └── main.js                                         // 程序入口
+├── favicon.ico                                         // 图标
+├── index.html                                          // 入口html
+.
+```
---
+++ src/page/deliveryAddress/children/addressAdd.vue
@@ -0,0 +1,266 @@
+<template>
+  <div class="addressAddPage">
+    <top-back title="添加地址"></top-back>
+    <section class="addressDetail">
+      <section class="name">
+        <h4>联系人</h4>
+        <input v-model="name" type="text" placeholder="你的姓名">
+      </section>
+      <section class="sex">
+        <div class="sexItem">
+          <input v-model="sex" type="radio" value="先生" id="man">
+          <label for="man">先生</label>
+        </div>
+        <div class="sexItem">
+          <input v-model="sex" type="radio" value="女士" id="woman">
+          <label for="woman">女士</label>
+        </div>
+      </section>
+      <section class="phone">
+        <h4>电话</h4>
+        <input v-model="phone" type="text" placeholder="你的手机号">
+      </section>
+      <router-link
+        :to="{name: 'addressNearby'}"
+        tag="section"
+        event="touchend"
+        class="pos">
+        <h4>位置</h4>
+        <div class="posRight">
+          <input v-model="pos" type="text" placeholder="小区/写字楼/学校等">
+          <span class="arrows">&gt;</span>
+        </div>
+      </router-link>
+      <section class="addressMore">
+        <h4>详细地址</h4>
+        <textarea v-model="addressMore" placeholder="详细地址(如门牌号)" class="addressMoreText"></textarea>
+      </section>
+      <section class="tag">
+        <h4>标签</h4>
+        <section class="tagRight">
+          <div class="tagItem">
+            <input v-model="tag" type="radio" value="家" id="home">
+            <label for="home">家</label>
+          </div>
+          <div class="tagItem">
+            <input v-model="tag" type="radio" value="学校" id="school">
+            <label for="school">学校</label>
+          </div>
+          <div class="tagItem">
+            <input v-model="tag" type="radio" value="公司" id="company">
+            <label for="company">公司</label>
+          </div>
+        </section>
+      </section>
+      <v-touch
+        tag="footer"
+        class="confirm"
+        @tap="confirm">确定</v-touch>
+    </section>
+    <matte-opacity v-if="popup"></matte-opacity>
+    <pop-up v-if="popup" :warnMessage="warnMessage" @warnConfirm="warnConfirm"></pop-up>
+  </div>
+</template>
+
+<script>
+import TopBack from '../../../components/common/TopBack'
+import {addressAdd} from '../../../service/getData'
+import {mapState, mapGetters, mapMutations} from 'vuex'
+
+export default {
+  name: 'AddressAdd',
+
+  components: {
+    TopBack,
+    MatteOpacity: () => import('../../../components/common/MatteOpacity'),
+    PopUp: () => import('../../../components/common/PopUp')
+  },
+
+  data () {
+    return {
+      name: '',
+      sex: '',
+      phone: '',
+      pos: '',
+      addressMore: '',
+      tag: '',
+      popup: false,
+      warnMessage: ''
+    }
+  },
+
+  computed: {
+    ...mapState([
+      'userInfo',
+      'addressTS'
+    ]),
+
+    ...mapGetters([
+      'geohash'
+    ])
+  },
+
+  beforeRouteEnter (to, from, next) { // 从addressNEarby回到此页前,从store获取数据，填写表单
+    if (from.name === 'addressNearby') {
+      next(vm => {
+        if (Object.keys(vm.addressTS).length > 0) {
+          ['name', 'sex', 'phone', 'pos', 'addressMore', 'tag'].forEach((item, index) => {
+            if (vm.addressTS[item]) vm[item] = vm.addressTS[item]
+          })
+        }
+      })
+    } else {
+      next()
+    }
+  },
+
+  beforeRouteLeave (to, from, next) { // 离开此页去addressNearby页时保存填写的表单数据
+    if (to.name === 'addressNearby') {
+      this.addressTemporaryDataSave({
+        name: this.name,
+        sex: this.sex,
+        phone: this.phone,
+        pos: this.pos,
+        addressMore: this.addressMore,
+        tag: this.tag
+      })
+    }
+    next()
+  },
+
+  methods: {
+    async confirm () {
+      if (!this.name) {
+        this.popup = true
+        this.warnMessage = '请输入您的名字'
+      } else if (!this.phone) {
+        this.popup = true
+        this.warnMessage = '请输入手机号'
+      } else if (!this.pos) {
+        this.popup = true
+        this.warnMessage = '请输入地址'
+      } else if (!this.addressMore) {
+        this.popup = true
+        this.warnMessage = '请输入详细地址'
+      } else if (!this.userInfo) {
+        this.popup = true
+        this.warnMessage = '请先登录'
+      } else {
+        let res = await this.addressAdd()
+        if (res.status === 0) {
+          this.popup = true
+          this.warnMessage = res.message
+        } else {
+          this.$router.push({name: 'deliveryAddress'})
+        }
+      }
+    },
+
+    async addressAdd () {
+      let res = await addressAdd(this.userInfo.user_id, this.addressMore, this.pos, this.geohash, this.name, this.phone, this.sex, this.tag)
+      return res
+    },
+
+    warnConfirm () {
+      this.popup = false
+    },
+
+    ...mapMutations([
+      'addressTemporaryDataSave'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .addressAddPage {
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    left: 0;
+    top: 0;
+    background-color: #ebebeb;
+    .addressDetail {
+      h4 {
+        font-size: .14rem;
+        font-weight: bold;
+        color: #202020;
+        width: 20%
+      }
+      input {
+        font-size: .14rem;
+        color: #707070;
+      }
+      .name, .sex, .phone, .pos, .addressMore, .tag {
+        display: flex;
+        align-items: center;
+        background-color: #fff;
+        margin: 1px 0;
+        padding: .15rem;
+        .addressMoreText {
+          font-size: .13rem;
+          color: #707070;
+        }
+        .posRight {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          width: 80%;
+        }
+        .arrows {
+          font-size: .16rem;
+          color: #8a8a8a;
+          transform: scale(1, 2);
+        }
+      }
+      .name, .phone, .addressMore {
+        input, textarea {
+          width: 80%;
+        }
+      }
+      .sex {
+        .sexItem {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex: auto;
+          color: #3a3a3a;
+          font-size: .14rem;
+          label {
+            margin-left: .05rem;
+          }
+        }
+      }
+      .tag {
+        .tagRight {
+          display: flex;
+          align-items: center;
+          width: 80%;
+          .tagItem {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex: auto;
+            font-size: .14rem;
+            color: #3a3a3a;
+            label {
+              margin-left: .05rem;
+            }
+          }
+        }
+      }
+      .confirm {
+        width: 95%;
+        background-color: #2dc954;
+        font-size: .16rem;
+        color: #fff;
+        padding: .1rem 0;
+        margin: 0 auto;
+        text-align: center;
+        border-radius: .05rem;
+        margin-top: .1rem;
+      }
+    }
+  }
+</style>
+
